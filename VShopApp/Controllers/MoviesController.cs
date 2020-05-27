b12772e210b1e24b8346ae810a28c65d17c628ec
Swathi Thada <swathithada3@gmail.com>
2020-05-27 12:02:34 -0400

    Add project files.

---
+++ VShopApp/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VShopApp.Models;
+
+namespace VShopApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //Movies
+        public ActionResult Index()
+        {
+            return HttpNotFound();
+            //return EmptyResult;
+        }
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Thappad" };
+            // return View(movie);
+            //return Content("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ VShopApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VShopApp.Startup))]
+namespace VShopApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
